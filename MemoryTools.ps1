14bc40640166ba5e74b61e293a1e995dcc6e58f0
jdhitsolutions <jhicks@jdhitsolutions.com>
2016-05-18 15:42:30 -0400

    added new functions

--- MemoryTools.ps1
+++ MemoryTools.ps1
@@ -1,9 +1,8 @@
 #requires -version 4.0
 
+#TO-DO
 #TURN THIS INTO A MODULE
-#FIX formatting for Show-MemoryUsage
-#create Test-memoryusage to run get-memory usage and test for a minimum %free,
-#FreeGB, TotalGB or UsedGB
+#create Test-memoryusage to run get-memory usage and test for a minimum %free, FreeGB, TotalGB or UsedGB
 #create help
 
 #region Define functions
@@ -58,9 +57,7 @@ End {
     Write-Verbose "Ending: $($MyInvocation.Mycommand)"
 } #end
 
-} #end function
-
-Set-Alias -Name gmu -Value Get-MemoryUsage
+} #end Get-MemoryUsage
 
 Function Show-MemoryUsage {
 
@@ -74,38 +71,38 @@ Param(
 
 Begin {
     Write-Verbose "Starting: $($MyInvocation.Mycommand)"  
+    #a formatted report title
     $title = @"
-
-Memory Check
-------------
+****************
+* Memory Check *
+****************
 
 "@
 
 Write-Host $title -foregroundColor Cyan
 
-([pscustomobject]@{
-Computername = ''
-Status = ''
-PctFree = ''
-FreeGB = ''
-TotalGB = ''
-} |format-table -autosize | out-string).Trim() | write-host -ForegroundColor Cyan
-
 } #begin
 
 Process {
-foreach ($computer in $computername) {
+#foreach ($computer in $computername) {
     #get memory usage data
-    $data = Test-MemoryUsage -Computername $computer
-
-    Switch ($data.Status) {
-    "OK" { $color = "Green" }
-    "Warning" { $color = "Yellow" }
-    "Critical" {$color = "Red" }
-    }
-    
-    ($data | Format-Table -AutoSize -HideTableHeaders | Out-String).Trim() | Write-Host -ForegroundColor $color
- } #foreach
+    $data = Get-MemoryUsage -Computername $computername
+    #create a text table and split into an array based on each line
+    $strings = ($data | Format-Table | Out-String).Trim().split("`n")
+    #display the first two lines which should be the header
+    $strings | select -first 2 | write-host -ForegroundColor Cyan
+    #process remaining lines
+    $strings | select -Skip 2 | foreach {
+        #check for the status and select an appropriate color
+        Switch -regex ($_) {
+        "OK" { $color = "Green" }
+        "Warning" { $color = "Yellow" }
+        "Critical" {$color = "Red" }
+        }
+        #write the line with the corresponding alert color
+        Write-Host $_ -ForegroundColor $color
+    } 
+ #} #foreach
 } #Process
 
 End {
@@ -114,20 +111,164 @@ End {
 
 } #end Show-MemUsage
 
-set-alias -Name smu -Value Show-MemoryUsage
+Function Get-MemoryPerformance {
 
-<#
-$counters = "\Memory\Available Bytes","\Memory\Committed Bytes","\Memory\% Committed Bytes In Use"
-$d = Get-Counter -Counter $counters
+[cmdletbinding()]
+Param(
+[Parameter(Position = 0,ValueFromPipeline)]
+[ValidateNotNullorEmpty()]
+[Alias("cn")]
+[string[]]$Computername = $env:Computername
+)
 
-#$d.CounterSamples[0].path.Split("\")[-1]
+Begin {
+    Write-Verbose "Starting: $($MyInvocation.Mycommand)"  
+    <#
+        Get all memory performance counters. Assuming counters on the 
+        client are the same as on the server. Sort by name.
+    #>
+    $all = (get-counter -ListSet Memory*).counter | Sort-Object
+} #begin
 
-$d.CounterSamples | Select @{Name="Counter";Expression={ $_.path.Split("\")[-1]}},
-@{Name="Value";Expression={$_.cookedValue}}
+Process {
+    foreach ($computer in $computername) {
+        Write-Verbose "Getting memory performance data from $Computer"
+        Try {
+            $data =  Get-Counter -Counter $all -computername $computer -ErrorAction Stop
+            if ($data.CounterSamples) {
+                $data.counterSamples | Select @{Name="Counter";Expression={ $_.path.Split("\")[-1]}},
+                @{Name="Value";Expression={$_.cookedValue}} | foreach -begin {
+             $h = [ordered]@{
+              Computername = $computer.ToUpper()
+              DateTime = (Get-Date)
+              }
+             } -process {
+             #replace any / or - with spaces
+             $rawname = $_.counter.replace("/"," ").replace("-"," ")
+             #make proper case
+             $proper = $rawname.split(" ").foreach({"$(([string]($_[0])).toUpper())$($_.substring(1))"})
+             #join to new word
+             $property = $proper -join ""
+             #add to the hash table
+             $h.Add($property,$_.Value)
+            } -end {
+                #turn the hashtable into an object
+                [pscustomobject]$h
+            }
+            } #if data
+        } #try
+        Catch {
+            Write-Error "Failed to get performance data from $($computer.toupper()). $($_.exception.message)"
+        }
+    } #foreach
 
-#>
+} #process
 
+End {
+    Write-Verbose "[END    ] Ending: $($MyInvocation.Mycommand)"
+} #end
+} #end Get-MemoryPerformance
 
+Function Test-MemoryUsage {
+#get-memory usage and test for a minimum %free, FreeGB, TotalGB or UsedGB
+[cmdletbinding(DefaultParameterSetName="Percent")]
+Param(
+[Parameter(Position = 0,ValueFromPipeline)]
+[ValidateNotNullorEmpty()]
+[Alias("cn")]
+[string[]]$Computername = $env:Computername,
+[Parameter(ParameterSetName="Percent")]
+[ValidateNotNullorEmpty()]
+[int]$PercentFree = 50,
+[Parameter(ParameterSetName="Free")]
+[ValidateNotNullorEmpty()]
+[double]$FreeGB,
+[ValidateNotNullorEmpty()]
+[Parameter(ParameterSetName="Total")]
+[int]$TotalGB,
+[Parameter(ParameterSetName="Used")]
+[ValidateNotNullorEmpty()]
+[double]$UsedGB,
+[switch]$Quiet
+)
 
+Begin {
+    Write-Verbose "Starting: $($MyInvocation.Mycommand)"  
+    Write-Verbose "Using parameter set $($PSCmdlet.ParameterSetName)"
+     Switch ($PSCmdlet.ParameterSetName) {
+            "Used"  { Write-Verbose "Testing if Used GB is >= to $UsedGB" }
+            "Total" { Write-Verbose "Testing if Total size is >= $TotalGB"  }
+            "Free"  { Write-Verbose "Testing if Free GB is >= $FreeGB" }
+            "Percent"  { Write-Verbose "Testing if Percent free is >= $PercentFree" }
+            } #switch
+} #begin
+Process {
+    foreach ($computer in $computername) {
+        Write-Verbose "Processing $computer"
+        Try {
+            $mem = Get-MemoryUsage -Computername $computer -ErrorAction Stop
+            Switch ($PSCmdlet.ParameterSetName) {
+            "Used"  {  
+                        $used = $mem.TotalGB - $mem.FreeGB
+                        if ($Used -ge $mem.usedGB) {
+                            $Test = $True
+                        }
+                        else {
+                            $Test = $False
+                        }
+                        $data = $mem | Select Computername,@{Name="UsedGB";Expression={$used}},
+                        @{Name="Test";Expression={$test}}
+                    }
+            "Total" {
+                        if ($mem.TotalGB -ge $TotalGB) {
+                            $Test = $True
+                        }
+                        else {
+                            $Test = $False
+                        }
+                        $data = $mem | Select Computername,TotalGB,@{Name="Test";Expression={$test}}
+                    }
+            "Free"  {
+                        if ($FreeGB -le $mem.FreeGB) {
+                            $Test = $True
+                        }
+                        else {
+                            $Test = $False
+                        }
+                        $data = $mem | Select Computername,FreeGB,@{Name="Test";Expression={$test}}
+                    }
+            "Percent"  {
+                        if ($mem.PctFree -ge $percentFree) {
+                            $Test = $True
+                        }
+                        else {
+                            $Test = $False
+                        }
+                        $data = $mem | Select Computername,PctFree,@{Name="Test";Expression={$test}}
+                        }
+            } #switch
+            
+            if ($Quiet) {
+                $Test
+            }
+            else {
+                $data
+            }
+        } #try
+        Catch {
+            Write-Warning "[$($Computer.toUpper())] $($_.exception.message)"
+        }
+    } #foreach
+} #process
+End {
+    Write-Verbose "[END    ] Ending: $($MyInvocation.Mycommand)"
+} #end
+
+} #end Test-MemoryUsage
 #endregion
 
+#define some aliases
+Set-Alias -Name shmem -Value Show-MemoryUsage
+Set-Alias -Name gmem -Value Get-MemoryUsage
+Set-Alias -Name gmemp -Value Get-MemoryPerformance
+Set-Alias -Name tmem -Value Test-MemoryUsage
